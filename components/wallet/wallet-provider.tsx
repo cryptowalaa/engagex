0cb410c818fbad7bf4564dcf53eb87e1850edbd8
cryptowalaa <160880121+cryptowalaa@users.noreply.github.com>
2026-03-30 01:44:21 +0500

    Update wallet-provider.tsx

--- components/wallet/wallet-provider.tsx
+++ components/wallet/wallet-provider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FC, ReactNode, useMemo } from 'react'
+import { ReactNode, useMemo } from 'react'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { getWallets, SOLANA_RPC_ENDPOINT } from '@/lib/solana/wallet-adapter'
@@ -11,19 +11,17 @@ interface Props {
   children: ReactNode
 }
 
-export const SolanaWalletProvider: FC<Props> = ({ children }) => {
+export function SolanaWalletProvider({ children }: Props) {
   // Auto-detect all injected wallets (Phantom, Backpack, etc.)
   const wallets = useMemo(() => getWallets(), [])
 
   return (
-    <>
-      <ConnectionProvider endpoint={SOLANA_RPC_ENDPOINT} children={undefined}>
-        <WalletProvider wallets={wallets} autoConnect children={undefined}>
-          <WalletModalProvider>
-            {children}
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
-    </>
+    <ConnectionProvider endpoint={SOLANA_RPC_ENDPOINT}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
